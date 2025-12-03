00c14d459dae04ea93f9f73fb1aa3c0b75178a74
jacobsteffenBYU <jacob.steffen@byu.edu>
2025-12-02 23:33:02 -0700

    Adding extra file

---
+++ solutions/extra_02_user_login_authentication.py
@@ -0,0 +1,15 @@
+# ===========================
+# USER LOGIN & AUTHENTICATION
+# ===========================
+
+'''
+OVERVIEW
+--------
+If you plan on creating web apps and some content/pages should only be seen by
+verified users, you'll want to look into how Streamlit handles authentication.
+
+That involves some setup beyond the scope of this class, but feel free to read
+up on it here:
+
+https://docs.streamlit.io/develop/concepts/connections/authentication
+'''
